653a04bb2e6fe0fcac43d01927f81e153a671df2
YadavDipak <yadavdipak2911@gmail.com>
2025-04-01 18:10:08 +0530

    Header file

---
+++ components/Header.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Image from "next/image";
+import { assets } from "@/assets/assets";
+
+const Header = () => {
+  return (
+    <>
+      <div className="bg-gray-200 py-5 px-5 md:px-12 lg:px-28">
+        <div className="flex justify-between items-center bg-white px-5 py-4 ">
+          <Image
+            src={assets.logo}
+            alt="Blogger Image"
+            width={180}
+            className="w-[130px] sm:w-auto"
+          />
+          <button className="flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]">
+            Get Started <Image src={assets.arrow} alt="" />
+          </button>
+        </div>
+        <div className="text-center my-8">
+          <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
+          <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit,
+            saepe voluptate architecto, vero, a ex fugiat neque tempora quisquam
+            laborum consequuntur animi cum nostrum molestias et alias placeat
+            aliquid perspiciatis!
+          </p>
+          <form className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]">
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              className="pl-4 outline-none"
+            />
+            <button
+              type="submit"
+              className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white"
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
